86a08fb9477ff16ed15fa771ce10f333626f409c
guizbr <guilherme1290@outlook.com.br>
2023-03-14 17:15:10 -0300

    add-componentes-input/button/select/modal

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -1,21 +1,26 @@
-import {
-  Card, Container, Header, InputSearchContainer, ListContainer,
-} from './styles';
+import { Link } from 'react-router-dom';
 
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
+import Modal from '../../components/Modal';
+
+import {
+  Card, Container, Header, InputSearchContainer, ListContainer,
+} from './styles';
 
 export default function Home() {
   return (
     <Container>
+      <Modal danger />
+
       <InputSearchContainer>
         <input type="text" placeholder="Pesquisar contato..." />
       </InputSearchContainer>
 
       <Header>
         <strong>3 contatos</strong>
-        <a href="/">Novo contato</a>
+        <Link to="/new">Novo contato</Link>
       </Header>
 
       <ListContainer>
@@ -37,49 +42,9 @@ export default function Home() {
           </div>
 
           <div className="actions">
-            <a href="/">
+            <Link to="/edit/123">
               <img src={edit} alt="Edit" />
-            </a>
-            <button type="button">
-              <img src={trash} alt="Delete" />
-            </button>
-          </div>
-        </Card>
-
-        <Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Guilherme Camargo</strong>
-              <small>insragram</small>
-            </div>
-            <span>guilherme@email.com</span>
-            <span>(15) 99999-9999</span>
-          </div>
-
-          <div className="actions">
-            <a href="/">
-              <img src={edit} alt="Edit" />
-            </a>
-            <button type="button">
-              <img src={trash} alt="Delete" />
-            </button>
-          </div>
-        </Card>
-
-        <Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Guilherme Camargo</strong>
-              <small>insragram</small>
-            </div>
-            <span>guilherme@email.com</span>
-            <span>(15) 99999-9999</span>
-          </div>
-
-          <div className="actions">
-            <a href="/">
-              <img src={edit} alt="Edit" />
-            </a>
+            </Link>
             <button type="button">
               <img src={trash} alt="Delete" />
             </button>
--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -1,3 +1,12 @@
+import ContactForm from '../../components/ContactForm';
+import PageHeader from '../../components/PageHeader';
+
 export default function NewContact() {
-  return 'NewContactpage';
+  return (
+    <>
+      <PageHeader title="Novo Contato" />
+
+      <ContactForm buttonLabel="Cadastrar" />
+    </>
+  );
 }
